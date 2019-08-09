9d13cbabea61283cc08503b5e06f36daa1cb6429
ai-antena <53927513+ai-antena@users.noreply.github.com>
2019-08-09 13:05:40 +0900

    CIFAR10
    CIFAR10

---
+++ cifar10.py
@@ -0,0 +1,74 @@
+# ライブラリのインポート
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# train data
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+# test data
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# CNN
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(32, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.gap(x)
+        x = x.view(-1, 32)
+        x = self.fc(x)
+        return x
+
+
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+model = CNN().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+epoch_num = 100
+for epoch in range(epoch_num):
+    running_loss = 0.0
+    for i, (data, labels) in enumerate(trainloader):
+        data, labels = data.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    print('[%d] loss: %.3f' % (epoch + 1, running_loss / 50000))
+    running_loss = 0.0
+
+    # 学習中のモデルをテストデータ上で評価
+    if epoch % 1 ==0:
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for _, (data, labels) in enumerate(testloader):
+                data, labels = data.to(device), labels.to(device)
+                outputs = model(data)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
